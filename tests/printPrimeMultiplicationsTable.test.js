bc457a16a14ccee0cb47535a762c7ff6b86d7fc9
dbacall <dbacall@hotmail.co.uk>
2020-12-06 14:02:41 +0000

    all tests now passing, there was an extra space in tableString!!!!!!!

--- tests/printPrimeMultiplicationsTable.test.js
+++ tests/printPrimeMultiplicationsTable.test.js
@@ -21,7 +21,7 @@ describe('printPrimeMultiplicationTable function', () => {
   it('logs a 2 by 2 table', () => {
     sinon.stub(primeNumbersGeneratorModule, 'primeNumbersGenerator').returns([2])
     printPrimeMultiplicationTable(2)
-    const tableString = '|  |  2|\n| 2| 4|'
+    const tableString = '|  | 2|\n| 2| 4|'
     expect(console.log.calledWith(tableString)).to.be.true
   })
 
@@ -29,14 +29,15 @@ describe('printPrimeMultiplicationTable function', () => {
     sinon.stub(primeNumbersGeneratorModule, 'primeNumbersGenerator').returns([2, 3])
 
     printPrimeMultiplicationTable(3)
-    const tableString = '|  |  2| 3|\n| 2| 4| 6|\n| 3| 6| 9|'
+    const tableString = '|  | 2| 3|\n| 2| 4| 6|\n| 3| 6| 9|'
+
     expect(console.log.calledWith(tableString)).to.be.true
   })
 
   it('logs a 4 by 4 table', () => {
     sinon.stub(primeNumbersGeneratorModule, 'primeNumbersGenerator').returns([2, 3, 5])
     printPrimeMultiplicationTable(4)
-    const tableString = '|  |  2| 3| 5|\n| 2| 4| 6| 10|\n| 3| 6| 9| 15|\n| 5| 10| 15| 25|'
+    const tableString = '|  | 2| 3| 5|\n| 2| 4| 6| 10|\n| 3| 6| 9| 15|\n| 5| 10| 15| 25|'
     expect(console.log.calledWith(tableString)).to.be.true
   })
 })
