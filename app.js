ffd1547263080e54f23e1c7aef4a23bae8c2ca9a
dbacall <dbacall@hotmail.co.uk>
2020-12-06 12:59:54 +0000

    add ability for user to input number and see table in app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const printPrimeMultiplicationsTable = require('./lib/printPrimeMultiplicationsTable')
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question(`How big would you like your table to be? (Please enter one number, format will be number * number)`, (number) => {
+  printPrimeMultiplicationsTable(number)
+  readline.close()
+})
