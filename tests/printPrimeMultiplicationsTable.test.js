85e2a66d60372ad241e8da09842e9f91d5e669c6
dbacall <dbacall@hotmail.co.uk>
2020-12-06 12:06:08 +0000

    function now works for 3 by 3(tests not passing even though string is what expected)

--- tests/printPrimeMultiplicationsTable.test.js
+++ tests/printPrimeMultiplicationsTable.test.js
@@ -15,9 +15,15 @@ describe('printPrimeMultiplicationTable function', () => {
     console.log.restore();
   });
 
-  it.only('logs a 2 by 2 table', () => {
+  it('logs a 2 by 2 table', () => {
     printPrimeMultiplicationTable(2)
     const tableString = '|  |  2|\n| 2| 4|'
+    expect(console.log.calledWith(tableString)).to.be.true
+  })
+
+  it('logs a 3 by 3 table', () => {
+    printPrimeMultiplicationTable(3)
+    const tableString = '|  |  2| 3|\n| 2| 4| 6|\n| 3| 6| 9|'
     expect(console.log.calledWith(tableString)).to.be.true
   })
 })
