46809be1883efb83e289516dd68466f5ee89590e
dbacall <dbacall@hotmail.co.uk>
2020-12-06 10:42:52 +0000

    add test for first 15 primes

--- tests/primeNumbersGenerator.test.js
+++ tests/primeNumbersGenerator.test.js
@@ -19,4 +19,8 @@ describe('primeNumbersGenerator function', () => {
     expect(primeNumbersGenerator(7)).to.eql([2, 3, 5, 7, 11, 13, 17])
   })
 
+  it('returns an array with the first seven prime numbers', () => {
+    expect(primeNumbersGenerator(15)).to.eql([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47])
+  })
+
 })
